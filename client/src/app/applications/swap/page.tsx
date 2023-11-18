ecb5039ff3988584d856e5ea14e34e4f0dabd5ce
Théo Samarasinghe <teyik0@Ordinateur-portable-de-Theo.local>
2023-11-18 21:27:56 +0300

    using cowprotocol with wallet connect

--- client/src/app/applications/swap/page.tsx
+++ client/src/app/applications/swap/page.tsx
@@ -43,13 +43,40 @@ const Page = () => {
     };
   }, [provider]);
 
-  if (!params) {
-    return <div></div>;
-  }
+  const params2: CowSwapWidgetParams = {
+    appCode: 'My Cool App', // Name of your app (max 50 characters)
+    width: '100%', // Width in pixels (or 100% to use all available space)
+    height: '100%',
+    provider: provider, // Ethereum EIP-1193 provider. For a quick test, you can pass `window.ethereum`, but consider using something like https://web3modal.com
+    chainId: 1, // 1 (Mainnet), 5 (Goerli), 100 (Gnosis)
+    tradeType: TradeType.SWAP, // TradeType.SWAP, TradeType.LIMIT or TradeType.ADVANCED
+    sell: {
+      // Sell token. Optionally add amount for sell orders
+      asset: 'COW',
+      amount: '100000',
+    },
+    buy: {
+      // Buy token. Optionally add amount for buy orders
+      asset: 'USDC',
+      amount: '0',
+    },
+    enabledTradeTypes: [
+      // TradeType.SWAP, TradeType.LIMIT and/or TradeType.ADVANCED
+      TradeType.SWAP,
+      TradeType.LIMIT,
+      TradeType.ADVANCED,
+    ],
+    theme: 'dark', // light/dark or provide your own color palette
+    interfaceFeeBips: '50', // Fill the form above if you are interested
+  };
 
   return (
     <div>
-      <CowSwapWidget params={params} />
+      {params ? (
+        <CowSwapWidget params={params} />
+      ) : (
+        <div>No wallet connected</div>
+      )}
     </div>
   );
 };
--- client/src/lib/useProvider.ts
+++ client/src/lib/useProvider.ts
@@ -1,50 +1,56 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-import { EthereumProvider } from '@cowprotocol/widget-lib'
+import { EthereumProvider } from '@cowprotocol/widget-lib';
 
-import { useAccount, useConfig, useWalletClient, useConnect } from 'wagmi'
+import { useAccount, useConfig, useWalletClient, useConnect } from 'wagmi';
 
-import EventEmitter from 'events'
+import EventEmitter from 'events';
 
-const onDisconnect = new EventEmitter()
+const onDisconnect = new EventEmitter();
 
 export function useProvider(): EthereumProvider | undefined {
-  const { isDisconnected } = useAccount()
-  const connector = useConnect()
-  const [provider, setProvider] = useState<EthereumProvider | undefined>(undefined)
+  const { isDisconnected } = useAccount();
+  const connector = useConnect();
+  const [provider, setProvider] = useState<EthereumProvider | undefined>(
+    undefined
+  );
 
   useEffect(() => {
-    if (!connector || !connector.connectors.length || provider) return
+    if (!connector || !connector.connectors.length || provider) return;
+    console.log('connector.connectors', connector.connectors);
 
     // change 0 or 1 to change the provider
-    connector.connectors[1].getProvider().then((newProvider) => {
-        setProvider(getEthereumProvider(newProvider, onDisconnect))
-      })
-  }, [connector, provider])
+    connector.connectors[0].getProvider().then((newProvider) => {
+      setProvider(getEthereumProvider(newProvider, onDisconnect));
+    });
+  }, [connector, provider]);
 
   useEffect(() => {
-    if (!provider || !isDisconnected) return
+    if (!provider || !isDisconnected) return;
 
-    onDisconnect.emit('disconnect')
-  }, [provider, isDisconnected])
+    onDisconnect.emit('disconnect');
+  }, [provider, isDisconnected]);
 
-  return provider
+  return provider;
 }
 
-function getEthereumProvider(provider: EthereumProvider, onDisconnect: EventEmitter): EthereumProvider {
+function getEthereumProvider(
+  provider: EthereumProvider,
+  onDisconnect: EventEmitter
+): EthereumProvider {
   return {
     request(...args) {
-      return provider.request(...args)
+      return provider.request(...args);
     },
     enable(...args) {
-      return provider.enable(...args)
+      return provider.enable(...args);
     },
     on(event: string, args: never) {
       if (event === 'disconnect' || event === 'close') {
-        return onDisconnect.on('disconnect', args)
+        return onDisconnect.on('disconnect', args);
       } else {
-        return provider.on(event, args)
+        return provider.on(event, args);
       }
     },
-  }
+  };
 }
