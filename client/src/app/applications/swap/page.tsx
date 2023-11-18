1bf47dd75e0a617354acf3dd0c79d61c5db66c18
Théo Samarasinghe <teyik0@Ordinateur-portable-de-Theo.local>
2023-11-18 22:10:57 +0300

    cowProtocol working with metamask and wallet connect

--- client/src/app/applications/swap/page.tsx
+++ client/src/app/applications/swap/page.tsx
@@ -7,9 +7,11 @@ import {
   CowSwapWidgetParams,
   TradeType,
 } from '@cowprotocol/widget-react';
+import { useAccount, useConnect } from 'wagmi';
 
 const Page = () => {
   const provider = useProvider();
+  const { isDisconnected, connector } = useAccount();
 
   const params: CowSwapWidgetParams | null = useMemo(() => {
     if (!provider) {
@@ -41,41 +43,16 @@ const Page = () => {
       theme: 'dark', // light/dark or provide your own color palette
       interfaceFeeBips: '50', // Fill the form above if you are interested
     };
-  }, [provider]);
-
-  const params2: CowSwapWidgetParams = {
-    appCode: 'My Cool App', // Name of your app (max 50 characters)
-    width: '100%', // Width in pixels (or 100% to use all available space)
-    height: '100%',
-    provider: provider, // Ethereum EIP-1193 provider. For a quick test, you can pass `window.ethereum`, but consider using something like https://web3modal.com
-    chainId: 1, // 1 (Mainnet), 5 (Goerli), 100 (Gnosis)
-    tradeType: TradeType.SWAP, // TradeType.SWAP, TradeType.LIMIT or TradeType.ADVANCED
-    sell: {
-      // Sell token. Optionally add amount for sell orders
-      asset: 'COW',
-      amount: '100000',
-    },
-    buy: {
-      // Buy token. Optionally add amount for buy orders
-      asset: 'USDC',
-      amount: '0',
-    },
-    enabledTradeTypes: [
-      // TradeType.SWAP, TradeType.LIMIT and/or TradeType.ADVANCED
-      TradeType.SWAP,
-      TradeType.LIMIT,
-      TradeType.ADVANCED,
-    ],
-    theme: 'dark', // light/dark or provide your own color palette
-    interfaceFeeBips: '50', // Fill the form above if you are interested
-  };
+  }, [provider, connector?.id]);
 
   return (
     <div>
-      {params ? (
+      {params && connector ? (
         <CowSwapWidget params={params} />
       ) : (
-        <div>No wallet connected</div>
+        <div className='min-h-[80vh] flex justify-center items-center'>
+          Connect your wallet to swap
+        </div>
       )}
     </div>
   );
--- client/src/lib/useProvider.ts
+++ client/src/lib/useProvider.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 
 import { EthereumProvider } from '@cowprotocol/widget-lib';
 
@@ -8,26 +8,48 @@ import EventEmitter from 'events';
 
 const onDisconnect = new EventEmitter();
 
+const getUsedConnectorId = (connector: string) => {
+  switch (connector) {
+    case 'walletConnect':
+      return 0;
+    case 'injected':
+      return 1;
+    default:
+      return 0;
+  }
+};
+
 export function useProvider(): EthereumProvider | undefined {
-  const { isDisconnected } = useAccount();
+  const { isDisconnected, connector: usedConnector } = useAccount();
+
+  useEffect(() => {
+    console.log('connector.connectors', usedConnector);
+  }, [usedConnector]);
+
   const connector = useConnect();
   const [provider, setProvider] = useState<EthereumProvider | undefined>(
     undefined
   );
 
   useEffect(() => {
-    if (!connector || !connector.connectors.length || provider) return;
-    console.log('connector.connectors', connector.connectors);
-
+    if (
+      !connector ||
+      !connector.connectors.length ||
+      provider ||
+      !usedConnector?.id
+    )
+      return;
+    console.log('connector.FEJNLZNVNZV', usedConnector);
     // change 0 or 1 to change the provider
-    connector.connectors[0].getProvider().then((newProvider) => {
-      setProvider(getEthereumProvider(newProvider, onDisconnect));
-    });
-  }, [connector, provider]);
+    connector.connectors[getUsedConnectorId(usedConnector.id)]
+      .getProvider()
+      .then((newProvider) => {
+        setProvider(getEthereumProvider(newProvider, onDisconnect));
+      });
+  }, [connector, provider, usedConnector?.id]);
 
   useEffect(() => {
     if (!provider || !isDisconnected) return;
-
     onDisconnect.emit('disconnect');
   }, [provider, isDisconnected]);
 
